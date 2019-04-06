42ec5c400e7d565b554b2f570f941010e44eaf2f
TrevJonez <trevorjones141@gmail.com>
2019-04-06 16:55:29 -0600

    more automated sanity checks

---
+++ app/src/test/java/polyadapter/sample/SampleActivityTest.kt
@@ -0,0 +1,72 @@
+package polyadapter.sample
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.*
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.matcher.ViewMatchers.assertThat
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import io.reactivex.plugins.RxJavaPlugins
+import io.reactivex.schedulers.Schedulers
+import org.hamcrest.Matchers
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import polyadapter.PauseableScheduler
+import polyadapter.ShampooRule
+import java.util.concurrent.TimeoutException
+
+@RunWith(AndroidJUnit4::class)
+class SampleActivityTest {
+  private val pausingCompScheduler = PauseableScheduler()
+
+  @Before
+  fun setUp() {
+    pausingCompScheduler.pause()
+    IdlingRegistry.getInstance().register(pausingCompScheduler.idlingResource)
+    RxJavaPlugins.setComputationSchedulerHandler { pausingCompScheduler }
+  }
+
+  @After
+  fun tearDown() {
+    IdlingRegistry.getInstance().unregister(pausingCompScheduler.idlingResource)
+    RxJavaPlugins.reset()
+  }
+
+  @Test
+  fun `Default empty list first then items applied successfully`() {
+    ActivityScenario.launch(SampleActivity::class.java).use {
+      onView(withId(R.id.recycler))
+          .check(adapterItemCount(0))
+
+      pausingCompScheduler.resume()
+      pausingCompScheduler.idlingResource.waitForComputationFinish()
+
+      onView(withId(R.id.recycler))
+          .check(adapterItemCount(13))
+    }
+  }
+
+  private fun IdlingResource.waitForComputationFinish(sleepMillis: Long = 10, timeoutMillis: Long = 2000) {
+    var sleepTime = 0L
+    while (!isIdleNow) {
+      Thread.sleep(sleepMillis)
+      sleepTime += sleepMillis
+      if (sleepTime >= timeoutMillis)
+        throw TimeoutException("IdlingResource \"$name\" failed to idle after ${sleepTime}ms")
+    }
+  }
+
+  private fun adapterItemCount(expected: Int): ViewAssertion {
+    return ViewAssertion { view, noViewFoundException ->
+      noViewFoundException?.let { throw it }
+      val recycler = view as RecyclerView
+      val adapter = requireNotNull(recycler.adapter)
+
+      assertThat("Wrong adapter item count", adapter.itemCount, Matchers.`is`(expected))
+    }
+  }
+}
